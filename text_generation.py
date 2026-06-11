e3abfa3b8de4af4d08958dbd72161ab45d055480
Kishor Diwate <diwatekishor5@gmail.com>
2026-06-11 20:54:32 +0530

    Add text generation script using GPT-2

---
+++ text_generation.py
@@ -0,0 +1,13 @@
+!pip install transformers
+
+from transformers import pipeline
+
+generator = pipeline("text-generation", model="gpt2")
+
+output = generator(
+    "ai get job human in feture",
+    max_length=50,
+    num_return_sequences=1
+)
+
+print(output[0]["generated_text"])
